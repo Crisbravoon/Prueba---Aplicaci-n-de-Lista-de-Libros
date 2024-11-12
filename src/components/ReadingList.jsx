a3fc2a559cca609dcf6e3f625d0635f52ca2e9ee
Crisbravoon <cristobal_bravo6@hotmail.com>
2024-11-11 23:43:56 -0300

    Reducer Eliminar se agrego

--- src/components/ReadingList.jsx
+++ src/components/ReadingList.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
+
+import React from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { deletedReadingList } from '../features/books/bookSlice';
 
 const ReadingList = () => {
 
-const readingList = useSelector(state => state.books.readingList);
+    const readingList = useSelector(state => state.books.readingList);
+
+    const dispatch = useDispatch();
+
+    const deleteBookReadingList = (id) => {
+        dispatch(deletedReadingList(id));
+    };
 
     return (
         <form className="bg-purple-900 grid items-center justify-items-center">
@@ -13,6 +22,8 @@ const readingList = useSelector(state => state.books.readingList);
                     <div className="flex flex-col items-center justify-center w-1/5 sm:w-1/3 md:w-1/3" key={item.book.ISBN}>
                         <img className="rounded" style={{ width: '150px', height: '200px' }} src={item.book.cover} alt={item.book.title} />
                         {item.book.title} - {item.book.author.name}
+                        <button className='btn btn-danger '
+                            onClick={() => deleteBookReadingList(item.book.ISBN)}>Eliminar</button>
                     </div>
                 ))}
             </div>
--- src/features/books/bookSlice.js
+++ src/features/books/bookSlice.js
@@ -16,13 +16,17 @@ export const bookSlice = createSlice({
 
             //Guardamos los libros que se leeran.
             const bookToAdd = action.payload;
-            
+
             //Validamos si el ID del book no son iguales, de no ser así lo agrega a readingList.
-            if(!state.readingList.find(book => book.book.ISBN === bookToAdd.book.ISBN)){
+            if (!state.readingList.find(book => book.book.ISBN === bookToAdd.book.ISBN)) {
                 state.readingList.push(bookToAdd);
             }
-        }},
+        },
+        deletedReadingList: (state, action) => {
+            state.readingList = state.readingList.filter(book => book.book.ISBN !== action.payload);
+        },
+    },
 });
 
-export const { addToReadingList } = bookSlice.actions;
+export const { addToReadingList, deletedReadingList } = bookSlice.actions;
 export default bookSlice.reducer;
